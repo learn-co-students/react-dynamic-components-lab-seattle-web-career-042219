2580367d4c275c1dab936380226a190cc9e0ccc9
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-24 16:51:35 -0700

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,18 @@
-// add Comment component here
+
+import React, { Component } from 'react'
+
+
+export default class Comment extends Component {
+    constructor(props) {
+        super(props);
+       
+        this.state = { commentText: "" };
+    
+      }
+    render(){
+        return(
+        <div className="comment"> 
+            {this.props.commentText}
+        </div>) 
+    }
+}
